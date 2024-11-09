7180587a24b77b1c41a67069b1ddf11043c8654b
sanoops123 <sansanoop01@gmail.com>
2024-11-09 16:56:39 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://frontend-virid-one-14.vercel.app"],
+  origin: ["http://localhost:5173", "https://frontend-virid-one-14.vercel.app","https://frontend-ktcdua1dl-sanoops123s-projects.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"] 

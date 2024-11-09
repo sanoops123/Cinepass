f7a6d552e23e10db0546a0ec53914b129424ec79
sanoops123 <sansanoop01@gmail.com>
2024-11-09 16:36:31 +0530

    edited index

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://frontend-virid-one-14.vercel.app/"],
+  origin: ["http://localhost:5173", "https://frontend-virid-one-14.vercel.app"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"] 
 }));
 app.use(cookieParser());
 

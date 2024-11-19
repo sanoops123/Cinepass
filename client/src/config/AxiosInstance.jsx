ef02c2aeeb968c41ac73d156a984efe9ed24d22b
sanoops123 <sansanoop01@gmail.com>
2024-11-19 13:34:41 +0530

    CORS

--- client/src/config/AxiosInstance.jsx
+++ client/src/config/AxiosInstance.jsx
@@ -1,21 +1,9 @@
-/*
+
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL_LOCAL
 
 export const AxiosInstance =axios.create({
   baseURL: `${API_URL}/api`,
   withCredentials:true
-});
-*/
-import axios from "axios";
-
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_API_URL_LOCAL
-    : import.meta.env.VITE_API_URL_PRODUCTION;
-
-export const AxiosInstance = axios.create({
-  baseURL: `${API_URL}/api`,
-  withCredentials: true,
 });

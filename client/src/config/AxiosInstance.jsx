09aaa6b45408cee6b189ed37d5663e68ff71eac3
sanoops123 <sansanoop01@gmail.com>
2024-11-19 13:37:46 +0530

    VE

--- client/src/config/AxiosInstance.jsx
+++ client/src/config/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL_LOCAL
+const API_URL = import.meta.env.VITE_API_URL
 
 export const AxiosInstance =axios.create({
   baseURL: `${API_URL}/api`,

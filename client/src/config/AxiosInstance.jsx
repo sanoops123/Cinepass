35f0f8497811dbed0367c830025412104802e2f8
sanoops123 <sansanoop01@gmail.com>
2024-11-19 13:24:04 +0530

    ver

--- client/src/config/AxiosInstance.jsx
+++ client/src/config/AxiosInstance.jsx
@@ -1,10 +1,21 @@
-
+/*
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL
 
-
 export const AxiosInstance =axios.create({
   baseURL: `${API_URL}/api`,
   withCredentials:true
+});
+*/
+import axios from "axios";
+
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL_LOCAL
+    : import.meta.env.VITE_API_URL_PRODUCTION;
+
+export const AxiosInstance = axios.create({
+  baseURL: `${API_URL}/api`,
+  withCredentials: true,
 });
